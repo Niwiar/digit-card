8c227b335983a288128a3198bbf4945dfd5d3700
Niwiar-Priva <87194989+Niwiar@users.noreply.github.com>
2022-08-02 16:46:26 +0700

    add dashboard and card link

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { isCardAuth, isCard } = require('./middleware/checkCard');
+const { ifNotLoggedIn, ifLoggedIn, isAuth } = require('./middleware/checkUser');
 
 router.get('/', isCard, async (req, res) => {
     res.render('index.ejs');
@@ -10,16 +11,16 @@ router.get('/manage/:CardTag', isCardAuth, (req, res) => {
     res.render('card-edit');
 })
 
-// router.get('/login', ifLoggedIn, (req, res, next) => {
-//     res.render('login.ejs');
-// });
+router.get('/login', ifLoggedIn, (req, res, next) => {
+    res.render('login.ejs');
+});
 
-// router.get('/register', ifLoggedIn, (req, res, next) => {
-//     res.render('register.ejs');
-// });
+router.get('/register', ifLoggedIn, (req, res, next) => {
+    res.render('register.ejs');
+});
 
-// router.get('/manager', (req, res) => {
-//     res.render('card-template')
-// })
+router.get('/card_manager', ifNotLoggedIn, (req, res) => {
+    res.render('card-manager')
+})
 
 module.exports = router
