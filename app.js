92e3532e19ca6b10bc126e7dbfb056e33d55b391
Niwiar-Priva <87194989+Niwiar@users.noreply.github.com>
2023-06-09 11:57:09 +0700

    domain offset 3

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(
 );
 
 // only in local
-app.set('subdomain offset', 4);
+app.set('subdomain offset', 3);
 
 app.use(wildcardSubdomains({ namespace: 's', whitelist: ['www'] }));
 
--- cardApp.js
+++ cardApp.js
@@ -36,7 +36,7 @@ cardApp.use(
   })
 );
 
-cardApp.set('subdomain offset', 4);
+cardApp.set('subdomain offset', 3);
 
 cardApp.use(wildcardSubdomains({ namespace: 's', whitelist: ['www'] }));
 
