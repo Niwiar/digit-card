970e9714a8a460e8d730184081ba0171fe189cc0
rew17p49 <106513736+rew17p49@users.noreply.github.com>
2024-07-16 11:02:52 +0700

    update webcam

--- assets/js/webcam.js
+++ assets/js/webcam.js
@@ -1,57 +1,54 @@
 //Show Img
 function ShowImg() {
-    $.ajax({
-        url: "/card/data",
-        method: 'get',
-        cache: false,
-        success: function (response) {
-            var obj = JSON.parse(response);
-            console.log(obj)
-            document.getElementById('Show-Img').src = obj.ImgPath;
-        }
-    })
+  $.ajax({
+    url: "/card/data",
+    method: "get",
+    cache: false,
+    success: function (response) {
+      var obj = JSON.parse(response);
+      console.log(obj);
+      document.getElementById("Show-Img").src = obj.ImgPath;
+    },
+  });
 }
 
 function setCamera() {
-    //set camera
-Webcam.set({
+  //set camera
+  Webcam.set({
     width: 320,
     height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
-});
-Webcam.attach( '#camera' );
-document.getElementById('results').innerHTML = '';
+    image_format: "jpeg",
+    jpeg_quality: 90,
+  });
+  Webcam.attach("#camera");
+  document.getElementById("results").innerHTML = "";
 }
 
 function closeCamera() {
-    Webcam.reset();
-    document.getElementById('results').innerHTML = '';
-    
+  Webcam.reset();
+  document.getElementById("results").innerHTML = "";
 }
 
 function take_snapshot() {
-    
-    Webcam.snap( function(data_url) {
-        document.getElementById('results').innerHTML = 
-         '<img id="imageprev" class="cropped1" src="'+data_url+'"/>';
-     } );
-     
-     Webcam.reset();
+  Webcam.snap(function (data_url) {
+    document.getElementById("results").innerHTML = '<img id="imageprev" class="cropped1" src="' + data_url + '"/>';
+  });
+
+  Webcam.reset();
 }
 
 function dataURItoBlob(dataURI) {
-    console.log(dataURI)
-    var byteString = atob(dataURI.split(',')[1]);  
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-  
-    var blob = new Blob([ab], {type: mimeString});
-    return blob;
+  console.log(dataURI);
+  var byteString = atob(dataURI.split(",")[1]);
+  var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  var blob = new Blob([ab], { type: mimeString });
+  return blob;
 }
 
 // $(document).ready(function () {
@@ -93,59 +90,65 @@ function dataURItoBlob(dataURI) {
 //             }
 //         })
 //         closeCamera();
-        
+
 //     })
 
-    
 // })
 
 $(document).ready(function () {
-    $('#btnConfirmImg').on('click',() => {
-        var dataUrl = $("#mobileImg").attr('src');
-        let blob = dataURItoBlob(dataUrl)
-        document.getElementById('showmobileImg').src = dataUrl;
-        let data = new FormData();
-        data.append('profile', blob, 'profile');
-        $.ajax({
-            url: '/card/upload/',
-            type: 'post',
-            data: data,
-            processData: false,
-            contentType: false,
-            success: function (data) {
-                Swal.fire({
-                    position: 'center',
-                    icon: 'success',
-                    title: 'Uploded',
-                    text: 'Successfully uploaded.',
-                    showConfirmButton: true,
-                    confirmButtonText: 'OK',
-                    confirmButtonColor: '#007bff',
-                    allowOutsideClick: false
-                })
-            },
-            error: function (err) {
-                errorText = err.responseJSON.message;
-                Swal.fire({
-                    position: 'center',
-                    icon: 'warning',
-                    title: 'Warning',
-                    text: errorText,
-                    showConfirmButton: true,
-                    confirmButtonText: 'OK',
-                    confirmButtonColor: '#FF5733'
-                });
-            }
+  $("#btnConfirmImg").on("click", () => {
+    let dataUrl = $("#mobileImg").attr("src");
+    console.log(dataUrl.includes("/imgs/profile"));
+
+    if (!dataUrl.includes("/imgs/profile")) {
+      let blob = dataURItoBlob(dataUrl);
+      document.getElementById("showmobileImg").src = dataUrl;
+      let data = new FormData();
+      data.append("profile", blob, "profile");
+      $.ajax({
+        url: "/card/upload/",
+        type: "post",
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Uploded",
+            text: "Successfully uploaded.",
+            showConfirmButton: true,
+            confirmButtonText: "OK",
+            confirmButtonColor: "#007bff",
+            allowOutsideClick: false,
+          });
+        },
+        error: function (err) {
+          errorText = err.responseJSON.message;
+          Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "Warning",
+            text: errorText,
+            showConfirmButton: true,
+            confirmButtonText: "OK",
+            confirmButtonColor: "#FF5733",
+          });
+        },
+      });
+      closeCamera();
+    }
+    else {
+        Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "Warning",
+            text: "รูปภาพไม่มีการเปลี่ยนแปลง",
+            showConfirmButton: true,
+            confirmButtonText: "OK",
+            confirmButtonColor: "#FF5733",
         })
-        closeCamera();
-        
-    })
-
-    
-})
-
-
-
-
-
-
+      closeCamera();
+    }
+  });
+});
